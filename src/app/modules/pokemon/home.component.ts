0e2412c49cf46e63bbc22a11587312b9e6056092
Stefano Giordano <giordano.stefano1995@gmail.com>
2021-10-26 19:25:26 +0200

    minor fix

--- src/app/modules/pokemon/home.component.ts
+++ src/app/modules/pokemon/home.component.ts
@@ -7,53 +7,53 @@ import { GetPokeService } from 'src/app/services/get-poke.service';
   selector: 'app-home',
   template: `
 
-  <div class="d-flex justify-content-center">
+<div class="d-flex justify-content-center">
 
-  <app-list-declined></app-list-declined>
+    <app-list-declined></app-list-declined>
 
 
     <ng-container *ngIf="pokemon | async as poke">
 
-      <div class="card me-5 ms-5" style="width: 18rem;">
-      <h1 class="card-title mb-5">{{poke.name}}</h1>
-       <img class="card-img-top" src="{{poke.sprites.front_default}}" alt="{{poke.name}}">
-        <div class="card-body">
-         <h5 class="mb-3 card-text">Dettagli:</h5>
-         <p class="card-text">Id: {{poke.id}}</p>
-         <p class="card-text">Weight: {{poke.weight}}</p>
-         <p class="card-text">Height: {{poke.height}}</p>
-         <div class="d-flex justify-content-between mb-3">
-         <button href="#" class="btn btn-success" (click)="capture(poke)" >Cattura</button>
-         <button href="#" class="btn btn-danger" (click)="reject(poke)">Rifiuta</button>
-         </div>
-         <p class="lol">E' apparso un {{poke.name | uppercase}} selvatico!</p>
-       </div>
-      </div>
+        <div class="card me-5 ms-5" style="width: 18rem;">
+            <h1 class="card-title mb-5">{{poke.name}}</h1>
+            <img class="card-img-top" src="{{poke.sprites.front_default}}" alt="{{poke.name}}">
+            <div class="card-body">
+                <h5 class="mb-3 card-text">Dettagli:</h5>
+                <p class="card-text">Id: {{poke.id}}</p>
+                <p class="card-text">Weight: {{poke.weight}}</p>
+                <p class="card-text">Height: {{poke.height}}</p>
+                <div class="d-flex justify-content-between mb-3">
+                    <button href="#" class="btn btn-success" (click)="capture(poke)">Cattura</button>
+                    <button href="#" class="btn btn-danger" (click)="reject(poke)">Rifiuta</button>
+                </div>
+                <p class="lol">E' apparso un {{poke.name | uppercase}} selvatico!</p>
+            </div>
+        </div>
     </ng-container>
 
     <app-list-accepted></app-list-accepted>
 
-    </div>
-  `,
+</div>
+`,
   styles: [
     `
-    .card{
-    
-        background-color: rgb(0,0,0);
-        background-color: rgba(0,0,0,0.79);
-    }
+.card{
 
-    .card-text{
-     color: white;
-    }
+background-color: rgb(0,0,0);
+background-color: rgba(0,0,0,0.79);
+}
 
-    .card-title{
-      color:white;
-    }
-    .lol{
-      color: yellow;
-    }
-    `
+.card-text{
+color: white;
+}
+
+.card-title{
+color:white;
+}
+.lol{
+color: yellow;
+}
+`
   ]
 })
 export class HomeComponent implements OnInit {
--- src/app/modules/pokemon/list-declined.component.ts
+++ src/app/modules/pokemon/list-declined.component.ts
@@ -5,22 +5,22 @@ import { GetPokeService } from 'src/app/services/get-poke.service';
 @Component({
   selector: 'app-list-declined',
   template: `
-  <h2>Pokemon Rifiutati: {{this.getService.declinedArr.length}}</h2>
-  <div class="overflow-auto" style="height:90vh;">
-  <ng-container *ngIf="this.getService.pokeRejected$ | async as poke">
-      <ng-container *ngFor="let data of poke">
-      <div class="alert alert-primary d-flex" role="alert" (click)="getDetails(data.id)">
-          <img src="{{data.sprites.front_default}}" alt="">
-          <p>#{{data.id}}</p>
-          <p class="ml-3">{{data.name}}</p>
-      </div>
-      </ng-container>
-  </ng-container>
-  </div>
-  `,
+<h2>Pokemon Rifiutati: {{this.getService.declinedArr.length}}</h2>
+<div class="overflow-auto" style="height:90vh;">
+    <ng-container *ngIf="this.getService.pokeRejected$ | async as poke">
+        <ng-container *ngFor="let data of poke">
+            <div class="alert alert-primary d-flex" role="alert" (click)="getDetails(data.id)">
+                <img src="{{data.sprites.front_default}}" alt="">
+                <p>#{{data.id}}</p>
+                <p class="ml-3">{{data.name}}</p>
+            </div>
+        </ng-container>
+    </ng-container>
+</div>
+`,
   styles: [
     `
-    `
+`
   ]
 })
 export class ListDeclinedComponent implements OnInit {
